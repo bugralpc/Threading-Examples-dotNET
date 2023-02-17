690652be1da953f3b6fb35cc3e8c6af7b2eef163
Buğra Alp Çevikgibi <bugra.cevikgibi@gmail.com>
2023-02-17 19:44:37 +0300

    Repository is constructed.

---
+++ ConsoleApp_Threading_1/ConsoleApp_Threading_1/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ConsoleApp_Threading_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(Method1);
+            Thread thread2 = new Thread(Method2);
+
+            thread1.Name = "Thread 1";
+            thread2.Name = "Thread 2";
+
+            thread1.Start();
+            thread2.Start();
+        }
+
+        static void Method1()
+        {
+            for (int i = 0; i < 2; i++)
+            {
+
+            }
+        }
+
+        static void Method2()
+        {
+            for(int i = 0; i < 2; i++)
+            {
+
+            }
+        }
+        
+
+    }
+}
