c8a08e356db3ad393650241367289cb9c16be49b
Buğra Alp Çevikgibi <bugra.cevikgibi@gmail.com>
2023-02-20 12:22:24 +0300

    Progress Bar with binding TextBlock is created.

---
+++ WpfApp_ThreadingBinding_1/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp_ThreadingBinding_1
+{
+    // We have to perform the task on a worker thread and then push updates to the UI thread.
+    // When running the main code, code automatically executes the WindowContentRendered method.
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void WindowContentRendered(object sender, EventArgs e)
+        {
+            // This below code block freeze the UI
+
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    progressBar.Value++;
+            //    Thread.Sleep(100);
+            //}
+
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.WorkerReportsProgress = true;
+            worker.DoWork += WorkerDoWork;
+            worker.ProgressChanged += WorkerProgressChanged;
+
+            worker.RunWorkerAsync();
+        }
+
+        private void WorkerDoWork(object sender, DoWorkEventArgs e)
+        {
+            for(int i = 0; i < 100; i++)
+            {
+                (sender as BackgroundWorker).ReportProgress(i);
+                Thread.Sleep(100);
+            }
+        }
+
+        private void WorkerProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressPercentage;
+        }
+    }
+
+
+}
